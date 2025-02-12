f72c0fb42dc65191a4fd12bf6cfad8d060f219c9
IsabelleRancan <isabellerancan@gmail.com>
2025-02-12 10:26:45 -0400

    Update upload_pictures.py

--- PortifolioFrameworkFlask/upload_pictures.py
+++ PortifolioFrameworkFlask/upload_pictures.py
@@ -1,21 +1,29 @@
 import os
-from datetime import datetime 
-from flask import current_app 
+from datetime import datetime
+from flask import current_app, request
+from werkzeug.utils import secure_filename
 
-UPLOAD_PICTURES = "/tmp/uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
-if not os.path.exists(UPLOAD_PICTURES):
-    os.makedirs(UPLOAD_PICTURES)
-
-def allowed_files(filename):
+def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def save_file(file):
-    if file and allowed_files(file.filename):
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
         timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
-        filename = f"{timestamp}_{file.filename}"
-        filepath = os.path.join(current_app.config["UPLOAD_FOLDER"], filename)
+        filename = f"{timestamp}_{filename}"
+        
+
+        if "VERCEL" in os.environ:
+            return f"Unable to generate files or folders, try running the local version of this application."  # Exemplo para URL de um serviço externo
+        
+
+        upload_folder = "uploads"
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+        
+        filepath = os.path.join(upload_folder, filename)
         file.save(filepath)
         return filepath
-    return None 
+    return None
